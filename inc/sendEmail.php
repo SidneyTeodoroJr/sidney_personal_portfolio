ed29ee58d0db0190a0f8fbe81161becc447644a5
Sidney Teodoro Araujo Araujo <sidneyteodoro2002@gmail.com>
2023-08-02 01:03:57 -0300

    php-alteration

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -3,8 +3,7 @@
 // Replace this with your own email address
 $siteOwnersEmail = 'sidneyteodoro2002@gmail.com';
 
-
-if($_POST) {
+if ($_POST) {
 
     $name = trim(stripslashes($_POST['contactName']));
     $email = trim(stripslashes($_POST['contactEmail']));
@@ -24,8 +23,9 @@ if($_POST) {
         $error['message'] = "Please enter your message. It should have at least 15 characters.";
     }
     // Subject
-    if ($subject == '') { $subject = "Contact Form Submission"; }
-
+    if ($subject == '') {
+        $subject = "Contact Form Submission";
+    }
 
     // Set Message
     $message .= "Email from: " . $name . "<br />";
@@ -39,31 +39,28 @@ if($_POST) {
 
     // Email Headers
     $headers = "From: " . $from . "\r\n";
-    $headers .= "Reply-To: ". $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
     if (!$error) {
 
         ini_set("sendmail_from", $siteOwnersEmail); // for windows server
         $mail = mail($siteOwnersEmail, $subject, $message, $headers);
 
-        if ($mail) { echo "OK"; }
-        else { echo "Something went wrong. Please try again."; }
-        
-    } # end if - no validation error
+        if ($mail) {
+            echo "OK";
+        } else {
+            echo "Something went wrong. Please try again.";
+        }
 
-    else {
+    } else {
 
         $response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
         $response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
         $response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
-        
+
         echo $response;
-
-    } # end if - there was a validation error
-
+    }
 }
-
 ?>
